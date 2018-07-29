50e719ed46ac3c9c7fc43692ede95d2708345495
songhee <>
2018-07-29 18:04:37 +0900

    백준 10973: 이전 순열

--- src/10973.java
+++ src/10973.java
@@ -1,6 +1,10 @@
+/* 1부터 N까지의 수로 이루어진 순열이 있다. 이 때, 사전순으로 바로 이전에 오는 순열을 구하는 프로그램을 작성하시오.
+사전 순으로 가장 앞서는 순열은 오름차순으로 이루어진 순열이고, 가장 마지막에 오는 순열은 내림차순으로 이루어진 순열이다. 
+첫째 줄에 입력으로 주어진 순열의 이전에 오는 순열을 출력한다. 만약, 사전순으로 가장 처음에 오는 순열인 경우에는 -1을 출력한다. */
+
 import java.util.Scanner;
 
-public class Main {
+public class 10973 {
 
 	static void swap(int[] arr, int n1, int n2) {
 		int tmp = arr[n1];
@@ -45,12 +49,12 @@ public class Main {
 				}
 			}
 
-			if (n2 + 1 == N) {
+			if (n1 + 1 == N - 1) {
 				for (int j = 0; j < arr.length; j++)
 					System.out.print(arr[j] + " ");
 				System.out.println();
 			} else {
-				int[] tmp = new int[N - n2];
+				int[] tmp = new int[N - n1 - 1];
 				int idx = n1 + 1;
 				for (int i = 0; i < tmp.length; i++) {
 					tmp[i] = arr[idx++];
