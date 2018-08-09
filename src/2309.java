f568448f42158335688ea0fe546e2dd86119fe99
songhee <20150261sh@gmail.com>
2018-08-10 03:28:24 +0900

    backtracking 구현 중간 완료

--- src/2309.java
+++ src/2309.java
@@ -9,6 +9,24 @@ public class Main {
    static ArrayList<Integer> heList = new ArrayList<>();
    static int total = 0;
 
+   static void back(int d, int idx, int total, int saving) {
+      if (d == 7) {
+         if (total == 100) {
+            for(int i = 0; i < 7; i++)
+               heList.add(save[i]);
+         } else if (total > 100) {
+            save[idx-1] = 0;
+            total -= height[idx++];
+            d--;
+         }
+         return;
+      } else {
+         for (; idx < 7; idx++) {
+            back(d + 1, idx + 1, total += height[idx], save[idx] = height[idx]);
+         }
+      }
+   }
+
    public static void main(String[] args) throws IOException {
       BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
       for (int i = 0; i < 9; i++) {
