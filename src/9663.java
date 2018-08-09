badb31efe3f72145fca75b92ceff1e52f431ffaa
songhee <20150261sh@gmail.com>
2018-08-10 03:24:51 +0900

    backtracking, promising 구현 완료

--- src/9663.java
+++ src/9663.java
@@ -5,7 +5,35 @@ import java.util.*;
 
 public class Main {
 	static int N;
+	static int cnt = 0;
 	static int[] col;
+	static int[] dx = { -1, -1 };
+	static int[] dy = { -1, 1 };
+
+	static boolean promising(int r) {
+		for (int i = 0; i < r; i++) {
+			if (col[i] == col[r])
+				return false;
+			if (Math.abs(col[i] - col[r]) == Math.abs(i - r))
+				return false;
+
+		}
+		return true;
+	}
+
+	static void back(int r) {
+		if (r == N) {
+			cnt++;
+		} else {
+			for (int i = 0; i < N; i++) {
+				col[r] = i;
+				if (promising(r))
+					back(r + 1);
+				else
+					col[r] = -1;
+			}
+		}
+	}
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -13,5 +41,6 @@ public class Main {
 		col = new int[N];
 		for (int i = 0; i < N; i++)
 			col[i] = -1;
+		
 	}
 }
