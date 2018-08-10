e903cea1b303e6ef17c8c8cde63c31922593aea7
songhee <20150261sh@gmail.com>
2018-08-10 22:49:55 +0900

    전체 구현 완료

--- src/2309.java
+++ src/2309.java
@@ -5,37 +5,38 @@ import java.util.*;
 
 public class Main {
    static int[] height = new int[9];
-   static int[] save = new int[7];
-   static ArrayList<Integer> heList = new ArrayList<>();
+   static int x1, x2;
    static int total = 0;
 
-   static void back(int d, int idx, int total, int saving) {
-      if (d == 7) {
-         if (total == 100) {
-            for(int i = 0; i < 7; i++)
-               heList.add(save[i]);
-         } else if (total > 100) {
-            save[idx-1] = 0;
-            total -= height[idx++];
-            d--;
+   static void findReal() {
+      for (int i = 0; i < 9; i++) {
+         total -= height[i];
+         for(int j = 1; j < 9; j++) {
+            if(total - height[j] == 100) {
+               x1 = i;
+               x2 = j;
+               return;
+            }
          }
-         return;
-      } else {
-         for (; idx < 7; idx++) {
-            back(d + 1, idx + 1, total += height[idx], save[idx] = height[idx]);
-         }
+         total += height[i];
       }
    }
 
    public static void main(String[] args) throws IOException {
       BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
       for (int i = 0; i < 9; i++) {
          height[i] = Integer.parseInt(br.readLine());
+         total += height[i];
       }
+
       Arrays.sort(height);
-      back(0, 0, total, 0);
-      for (int i = 0; i < 9; i++) {
-         System.out.println(heList.get(i));
+      findReal();
+      for(int i = 0; i < 9; i++) {
+         if(i == x1 || i == x2)
+            ;
+         else
+            System.out.println(height[i]);
       }
    }
 }
