73be5f070dae8216263948ba0807bd2afe612867
songhee <20150261sh@gmail.com>
2018-08-10 20:19:29 +0900

    입력 받기 구현 완료

---
+++ src/11399.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+	static int N;
+	static int[] P;
+	static int[] res;
+	static int time = 0;
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = null;
+
+		st = new StringTokenizer(br.readLine(), " ");
+		N = Integer.parseInt(st.nextToken());
+		P = new int[N];
+		res = new int[N];
+
+		st = new StringTokenizer(br.readLine(), " ");
+		for(int i = 0; i < N; i++) 
+			P[i] = Integer.parseInt(st.nextToken());
+	}
+}
