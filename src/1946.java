83bd443a10b73e83badb198d21f618a0abe8a884
songhee <20150261sh@gmail.com>
2018-08-14 09:46:28 +0900

    구현 완료

--- src/1946.java
+++ src/1946.java
@@ -3,11 +3,12 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
 
-public class source {
+public class Main {
 	static int T;
 	static int N;
 	static int[][] apply;
-	static int count = 0;
+	static int count = 1;
+	static int min = 0;
 
 	static void cruit() {
 		Arrays.sort(apply, new Comparator<int[]>() {
@@ -23,9 +24,13 @@ public class source {
 					return 0;
 			}
 		});
-		
-		for(int i = 1; i < N; i++) {
 
+		min = apply[0][1];		
+		for(int i = 1; i < N; i++) {
+			if(min > apply[i][1]) {
+				count++;
+				min = apply[i][1];
+			}
 		}
 	}
 
@@ -38,18 +43,15 @@ public class source {
 
 		while (T > 0) {
 			T--;
-			count = 0;
+			count = 1;
 			st = new StringTokenizer(br.readLine());
 			N = Integer.parseInt(st.nextToken());
 
-			int max = 0;
 			apply = new int[N][2];
 			for (int i = 0; i < N; i++) {
 				st = new StringTokenizer(br.readLine(), " ");
 				for (int j = 0; j < 2; j++) {
 					apply[i][j] = Integer.parseInt(st.nextToken());
-					if(apply[i][1] > max) 
-						max = apply[i][1];
 				}
 			}
 
