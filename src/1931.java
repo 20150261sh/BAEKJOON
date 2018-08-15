771a199c78c60d5e3202dee262877e389447c259
songhee <20150261sh@gmail.com>
2018-08-16 03:14:42 +0900

    자바 2차원 배열 정렬하는 부분 Comparator 사용으로 수정해서 구현 완료

--- src/1931.java
+++ src/1931.java
@@ -7,12 +7,12 @@ public class Main {
 	static int N;
 	static int[][] con;
 	static int count = 0;
-	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-	static StringTokenizer st = null;
 	static int end;
-	static int[] tmpArr;
 
 	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = null;
+
 		st = new StringTokenizer(br.readLine(), " ");
 		N = Integer.parseInt(st.nextToken());
 		con = new int[N][2];
@@ -26,39 +26,22 @@ public class Main {
 		Arrays.sort(con, new Comparator<int[]>() {
 
 			@Override
-			public int compare(int[] arg0, int[] arg1) {
+			public int compare(int[] up, int[] low) {
 				// TODO Auto-generated method stub
-				return arg0[1] - arg1[1];
+				if (up[1] == low[1]) { // 종료 시간이 같을 때 
+					return up[0] - low[0]; // 시작 시간으로 정렬 
+				} else { // 종료 시간이 다를 때 
+					return up[1] - low[1]; // 종료 시간으로 정렬 
+				}
 			}
 		});
 
-		int j;
-		for (int i = 0; i < N - 1; i++) {
-			if (con[i][1] == con[i + 1][1]) {
-				int tmp = con[i][1];
-				for (j = i + 1; j < N; j++) {
-					if (con[j][1] != tmp)
-						break;
-				}
-				tmpArr = new int[j - i];
-				int idx = 0;
-				for (int k = i; k < j; k++) {
-					tmpArr[idx++] = con[k][0];
-				}
-				Arrays.sort(tmpArr);
-				idx = 0;
-				for (int k = i; k < j; k++) {
-					con[k][0] = tmpArr[idx++];
-				}
-			}
-		}
-
 		end = con[0][1];
 		count++;
-		for (int i = 0; i < N; i++) {
+		for (int i = 1; i < N; i++) {
 			if (con[i][0] >= end) {
-				count++;
 				end = con[i][1];
+				count++;
 			}
 		}
 
