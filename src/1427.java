c36b9be264c2a671195c7c9c10882b6f83d8fd6d
songhee <20150261sh@gmail.com>
2018-08-20 13:58:06 +0900

    전체 구현 완료

--- src/1427.java
+++ src/1427.java
@@ -6,13 +6,6 @@ import java.util.*;
 public class Main {
 	static String N;
 
-	static Comparator<Integer> myComparator = new Comparator<Integer>() {
-
-		public int compare(Integer o1, Integer o2) {
-			return o2 - o1;
-		}
-	};
-
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
@@ -23,7 +16,9 @@ public class Main {
 		int[] arr = new int[spt.length];
 		for (int i = 0; i < spt.length; i++)
 			arr[i] = Integer.parseInt(spt[i]);
-
-		Collections.sort(arr, myComparator);
+		
+		Arrays.sort(arr);
+		for(int i = arr.length - 1; i > -1; i--)
+			System.out.print(arr[i]);
 	}
 }
